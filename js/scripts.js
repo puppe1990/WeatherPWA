2e3122642c21bfd6d9ce17ddab5b1340b11670b1
puppe1990 <matheus.puppe90@hotmail.com>
2018-07-23 02:09:50 -0300

    Weather now

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ function mudaCidade(cidade){
 
     document.querySelector(".row") != null ? document.querySelector(".row").remove() : null
     const URL = 'https://apiadvisor.climatempo.com.br/api/v1/forecast/locale/'+cidade+'/days/15?token=85d0b7516a7d9c2b89a0e3834cdaf37e';
-    console.log(URL);
-    console.log(cidade);
+    const URLagora = 'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/'+cidade+'/current?token=85d0b7516a7d9c2b89a0e3834cdaf37e';
+    console.log(URLagora);
     // const URL = 'js/dados.json';
     //instancia classe responsável ao consumo da api
     const xhr = new XMLHttpRequest();
@@ -24,6 +24,29 @@ function mudaCidade(cidade){
         const res = JSON.parse(xhr.response);
         console.log(res);
         construirQuadros(res);
+    }
+}
+
+function tempoAgora(cidade){
+
+    document.querySelector(".row") != null ? document.querySelector(".row").remove() : null
+    const URLagora = 'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/'+cidade+'/current?token=85d0b7516a7d9c2b89a0e3834cdaf37e';
+    // console.log(URLagora);
+    // const URL = 'js/dados.json';
+    //instancia classe responsável ao consumo da api
+    const xhr = new XMLHttpRequest();
+    //define opções da requisição
+    xhr.open('GET', URLagora, true);
+    //envia requisição para a api
+    xhr.send();
+    //trata os dados de maneira assincrona 
+    xhr.onload = function(e) {
+        //salva resposta da api em uma variavel
+        //e transforma resposta em um JSON válido
+        const res = JSON.parse(xhr.response);
+        console.log(res);
+        // alert("Tempo Agora em "+res.name+": "+ res.data.temperature+"C°");
+        construirQuadro(res);
     }
 }
 
@@ -84,3 +107,39 @@ function construirQuadros(dados){
     //adiciona a div no elemento main
     document.querySelector('main').appendChild(div);
 }    
+
+
+function construirQuadro(dados){
+    //cria elemento pai de cada card de dado
+    let div = document.createElement('div');
+    //adiciona classe row do bootstrap na div
+    div.classList.add('row');
+    //itera em cada um dos dados
+
+    const col = document.createElement('div');
+    col.classList.add('col-md-3', 'col-sm-6', 'col-12')
+    //cria card do bootstrap
+    const card = document.createElement('div');
+    card.classList.add('card', 'mb-3');
+    //adiciona event listener para card
+    col.appendChild(card);
+    //cria a div com a classe card-body que é onde
+    //o card do bootstrap mantém seu conteúdo
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    card.appendChild(cardBody);
+    //cria titulo em h5 utilizando a classe card-title
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+
+    //TO-DO: ADICIONE AQUI O NOME DOS POKEMONS
+    let elemento = document.createTextNode("Tempo Agora em " + dados.name + ":"+ dados.data.temperature + "C°");
+    cardTitle.appendChild(elemento);
+    cardBody.appendChild(cardTitle);
+
+    div.appendChild(col);
+
+    //adiciona a div no elemento main
+    document.querySelector('main').appendChild(div);
+}    
+
